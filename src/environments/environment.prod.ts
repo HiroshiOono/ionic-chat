74c91cb6be13cceaca3dd1ef12eebf1ca48cff66
hoono <95742535+HiroshiOono@users.noreply.github.com>
2021-12-21 19:48:03 +0900

    fix():チャットアプリ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyA9qtE84228dwK79JBB2T-iuoazOEQsiyU",
+    authDomain: "ionic-chat-fb3f4.firebaseapp.com",
+    databaseURL: "ionic-chat-.appspot.com",
+    projectId: "ionic-chat-fb3f4",
+    storageBucket: "ionic-chat-.appspot.com",
+    messagingSenderId: "1016060445252",
+    appId: "1:1016060445252:web:0555a64fb351e974f1f211"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyA9qtE84228dwK79JBB2T-iuoazOEQsiyU",
+    authDomain: "ionic-chat-fb3f4.firebaseapp.com",
+    databaseURL: "ionic-chat-.appspot.com",
+    projectId: "ionic-chat-fb3f4",
+    storageBucket: "ionic-chat-.appspot.com",
+    messagingSenderId: "1016060445252",
+    appId: "1:1016060445252:web:0555a64fb351e974f1f211"
+  }
 };
+
+
+
+
+
+
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
